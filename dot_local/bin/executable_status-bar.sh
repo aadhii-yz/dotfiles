ee8345dcc5dff6362dfc2b8746284e09a246e4a3
Adithya <aadhii.yz@gmail.com>
2025-12-07 15:30:57 +0530

    Configured status bar.

---
+++ dot_local/bin/executable_status-bar.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+
+set_status_bar() {
+  # Get Data
+  b="🔋: $(upower -i "$(upower -e | grep BAT)" | grep percentage | awk '{print $2}')"
+  d="📆: $(date +"%a %x")"
+  t="⏳: $(date +"%I:%M %p")"
+
+  # Set Format
+  status="[ $t | $d | $b ]"
+
+  # Set StatusBar
+  xprop -root -set WM_NAME "$status"
+}
+
+while true; do
+  set_status_bar
+  watch_bt
+  sleep 60
+done
